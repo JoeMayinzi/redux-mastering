7d62f3e039d497aac158db876f302e3ee9c01aeb
Joemayinzi <joemayinzi@gmail.com>
2023-06-09 15:05:49 +0100

    fix post reducer issues

--- src/actions/post.action.js
+++ src/actions/post.action.js
@@ -5,6 +5,7 @@ export const GET_POSTS = "GET_POSTS";
 export const getPosts = () => {
   return (dispatch) => {
     return axios.get("http://localhost:3000/posts").then((res) => {
+      console.log(res.data);
       dispatch({ type: GET_POSTS, payload: res.data });
     });
   };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
+import postReducer from "./postReducer";
 
 export default combineReducers({
   // All reducers of the whole === reducers container
   userReducer,
+  postReducer,
 });
